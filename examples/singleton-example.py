98f7527aac2d6c563bd0a030adede205013d9961
Zachary Cutlip <uid000@gmail.com>
2021-09-15 20:10:30 -0700

    add use of weakref to singleton example

--- examples/singleton-example.py
+++ examples/singleton-example.py
@@ -1,5 +1,6 @@
 import os
 import sys
+
 parent_path = os.path.dirname(
     os.path.dirname(
         os.path.abspath(__file__)
@@ -12,6 +13,10 @@ from pysingleton import PySingleton  # noqa: E402
 
 
 class MySingleton(metaclass=PySingleton):
+    # Set to True to allow the singleton instance
+    # to be deleted when it goes out of scope
+    # _PYSINGLETON_WEAKREF = True
+
     def __init__(self, value):
         self.value = value
 
@@ -38,3 +43,16 @@ print("my_singleton_1.value: {}".format(my_singleton_1.value))
 # First and second instance have the same object ID:
 print("id(my_singleton_1): {:#x}".format(id(my_singleton_1)))
 print("id(my_singleton_2): {:#x}".format(id(my_singleton_2)))
+
+ms_id_1 = id(my_singleton_1)
+
+# Delete current instances before creating a new one
+my_singleton_1 = my_singleton_2 = None
+
+# If MySingleton._PYSINGLETON_WEAKREF is set to True
+# or if MySingleton_WEAKREF environment variable is set to 1
+# the singleton will be destroyed, and my_singleton_3 will create a new instance
+my_singleton_3 = MySingleton(1)
+
+print(f"id(my_singleton_3): {id(my_singleton_3):#x}")
+print(f"my_singleton_1 == my_singleton_3 {ms_id_1 == id(my_singleton_3)}")
