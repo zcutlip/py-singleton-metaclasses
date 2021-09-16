7104da90d8079109d88300f558ea46f9a3f5387c
Zachary Cutlip <uid000@gmail.com>
2021-09-15 20:07:14 -0700

    apply same changes to PyMemoized

--- pysingleton/singleton.py
+++ pysingleton/singleton.py
@@ -32,12 +32,27 @@ class PySingleton(type):
 
 
 class PyMemoized(type):
-    _instances = {}
+    _instances = None
 
     def __call__(cls, memoized_key, *args, **kwargs):
+        if not cls._instances:
+            cls._initialize()
         instance = cls._instances.get((cls, memoized_key))
 
         if instance is None:
             instance = super().__call__(memoized_key, *args, **kwargs)
             cls._instances[(cls, memoized_key)] = instance
         return instance
+
+    def _initialize(cls):
+        use_weakref = getattr(cls, "_PYMEMOIZED_WEAKREF", None)
+        env_var = f"{cls.__name__}_WEAKREF"
+        if use_weakref is None:
+            use_weakref = os.getenv(env_var)
+            print(env_var)
+            print(f"use_weakref: {use_weakref}")
+            use_weakref = True if use_weakref == "1" else False
+        if use_weakref:
+            cls._instances = WeakValueDictionary()
+        else:
+            cls._instances = {}
