2149a5b921ca3eb9ea3e196eefbe3a7cc6382ec2
Zachary Cutlip <uid000@gmail.com>
2021-09-15 20:06:01 -0700

    allow PySingleton to use weak references for object deletion

--- pysingleton/singleton.py
+++ pysingleton/singleton.py
@@ -1,7 +1,14 @@
+import os
+
+from weakref import WeakValueDictionary
+
+
 class PySingleton(type):
-    _instances = {}
+    _instances = None
 
     def __call__(cls, *args, **kwargs):
+        if not cls._instances:
+            cls._initialize()
         instance = cls._instances.get(cls)
         if not instance:
             instance = super(
@@ -9,6 +16,19 @@ class PySingleton(type):
             ).__call__(*args, **kwargs)
             cls._instances[cls] = instance
         return instance
+
+    def _initialize(cls):
+        use_weakref = getattr(cls, "_PYSINGLETON_WEAKREF", None)
+        env_var = f"{cls.__name__}_WEAKREF"
+        if use_weakref is None:
+            use_weakref = os.getenv(env_var)
+            print(env_var)
+            print(f"use_weakref: {use_weakref}")
+            use_weakref = True if use_weakref == "1" else False
+        if use_weakref:
+            cls._instances = WeakValueDictionary()
+        else:
+            cls._instances = {}
 
 
 class PyMemoized(type):
